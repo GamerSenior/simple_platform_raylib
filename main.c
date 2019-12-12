633007fb5c8300fd7c8497945316a3f7b98817fd
Giovani Garcia <giovani.abel@supportweb.com.br>
2019-12-12 18:19:48 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "include/raylib.h"
+
+typedef struct {
+    bool mainMenu;
+    bool isPaused;
+    int menuOption;
+} World;
+
+void InputHandler(World *world);
+
+int main() {
+    // World State structure
+    World world = {
+            true,
+            false,
+            0,
+    };
+
+    SetTargetFPS(60);
+    // Main loop
+    while (!WindowShouldClose()) {
+        InputHandler(&world);
+    }
+}
+
+void InputHandler(World *world) {
+    if (world->mainMenu) {
+        if (IsKeyDown(KEY_DOWN)) {
+            if (world->menuOption != 0) {
+                world->menuOption++;
+                printf("%d", world->menuOption);
+            }
+        } else if (IsKeyDown(KEY_UP)) {
+            if (world->menuOption < 5) {
+                world->menuOption--;
+                printf("%d", world->menuOption);
+            }
+        }
+    }
+}
